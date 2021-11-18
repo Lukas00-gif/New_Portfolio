2d613dcfa6eaa379833bd8f46040c67cfc071703
Lukas00-gif <62776762+Lukas00-gif@users.noreply.github.com>
2021-11-18 17:59:09 -0300

    new portfolio online

---
+++ assets/js/main.js
@@ -0,0 +1,135 @@
+/*-------------------- MENU SHOW Y HIDDEN --------------------*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+/*----- MENU SHOW -----*/
+/* valida se a constante existe */
+if (navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+/*----- MENU HIDDEN -----*/
+/* valida se a constante existe */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu') // desepear the menu
+    })
+}
+/*-------------------- REMOVE MENU MOBILE --------------------*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // Quando clicamos em cada nav__link, removemos a classe show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*-------------------- SKILLS --------------------*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemclass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if (itemclass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
+/*-------------------- SERVICES MODAL --------------------*/
+
+
+/*-------------------- PORTFOLIO SWIPER  --------------------*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+/*-------------------- TESTIMONIAL --------------------*/
+
+
+/*-------------------- SCROLL SECTIONS ACTIVE LINK --------------------*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*-------------------- CHANGE BACKGROUND HEADER --------------------*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // Quando a rolagem é maior que 200 na altura da janela de visualização, adicione a classe scroll-header à tag de cabeçalho
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*-------------------- SHOW SCROLL UP --------------------*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // Quando a rolagem é superior a 560 de altura da janela de visualização, adicione a classe show-scroll à tag a com a classe scroll-top
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*-------------------- DARK LIGHT THEME --------------------*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-brightness'
+
+// Tópico selecionado anteriormente (se o usuário selecionou)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtemos o tema atual da interface validando a classe dark-theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-brightness'
+
+// Nós validamos se o usuário escolheu previamente um tópico
+if (selectedTheme) {
+  // Se a validação for cumprida, perguntamos qual era o problema para saber se ativamos ou desativamos o escuro
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Ative / desative o tema manualmente com o botão
+themeButton.addEventListener('click', () => {
+    // Adicionar ou remover o tema escuro / ícone
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // Nós salvamos o tema e o ícone atual que o usuário escolheu
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
