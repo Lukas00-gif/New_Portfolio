83e8265dfe353478d9b2041903f95a440b6bbc5d
Lukas00-gif <lucassky08@gmail.com>
2023-05-07 08:29:11 -0300

    alteracoes no skills e portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
 /*-------------------- MENU SHOW Y HIDDEN --------------------*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 
 /*----- MENU SHOW -----*/
 /* valida se a constante existe */
-if (navToggle){
+if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 /*----- MENU HIDDEN -----*/
 /* valida se a constante existe */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu') // desepear the menu
     })
 }
 /*-------------------- REMOVE MENU MOBILE --------------------*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // Quando clicamos em cada nav__link, removemos a classe show-menu
     navMenu.classList.remove('show-menu')
@@ -30,40 +30,40 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*-------------------- SKILLS --------------------*/
 const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('.skills__header')
+    skillsHeader = document.querySelectorAll('.skills__header')
 
-function toggleSkills(){
+function toggleSkills() {
     let itemclass = this.parentNode.className
 
-    for (i = 0; i < skillsContent.length; i++){
+    for (i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = 'skills__content skills__close'
     }
-    if (itemclass === 'skills__content skills__close'){
-    this.parentNode.className = 'skills__content skills__open'
+    if (itemclass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
     }
 }
 
-skillsHeader.forEach((el) =>{
+skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 
 /*-------------------- SERVICES MODAL --------------------*/
 const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalCloses = document.querySelectorAll('.services__modal-close')
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
 
-let modal = function(modalClick){
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add('active-modal')
 }
 
-modalBtns.forEach((modalBtn, i) =>{
+modalBtns.forEach((modalBtn, i) => {
     modalBtn.addEventListener('click', () => {
         modal(i)
     })
 })
 
-modalCloses.forEach((modalClose) =>  {
-    modalClose.addEventListener('click', () =>{
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
         modalViews.forEach((modalView) => {
             modalView.classList.remove('active-modal')
         })
@@ -77,12 +77,12 @@ let swiper = new Swiper(".portfolio__container", {
     loop: true,
 
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
 });
 
@@ -92,40 +92,40 @@ let swiper = new Swiper(".portfolio__container", {
 /*-------------------- SCROLL SECTIONS ACTIVE LINK --------------------*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
 
-/*-------------------- CHANGE BACKGROUND HEADER --------------------*/ 
-function scrollHeader(){
+/*-------------------- CHANGE BACKGROUND HEADER --------------------*/
+function scrollHeader() {
     const nav = document.getElementById('header')
     // Quando a rolagem é maior que 200 na altura da janela de visualização, adicione a classe scroll-header à tag de cabeçalho
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*-------------------- SHOW SCROLL UP --------------------*/ 
-function scrollUp(){
+/*-------------------- SHOW SCROLL UP --------------------*/
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // Quando a rolagem é superior a 560 de altura da janela de visualização, adicione a classe show-scroll à tag a com a classe scroll-top
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
-/*-------------------- DARK LIGHT THEME --------------------*/ 
+/*-------------------- DARK LIGHT THEME --------------------*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-brightness'
@@ -140,9 +140,9 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 
 // Nós validamos se o usuário escolheu previamente um tópico
 if (selectedTheme) {
-  // Se a validação for cumprida, perguntamos qual era o problema para saber se ativamos ou desativamos o escuro
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+    // Se a validação for cumprida, perguntamos qual era o problema para saber se ativamos ou desativamos o escuro
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Ative / desative o tema manualmente com o botão
@@ -154,5 +154,6 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
 
 
--- assets/js/staregg.js
+++ assets/js/staregg.js
@@ -1,30 +1,30 @@
 const codigoSecreto = "monument";
 // const imagemSecreta = document.querySelectorAll('.imagem_Secreta');
 
-    let Temporizador, Temp;
-    let SequenciaCaracteres = [];
+let Temporizador, Temp;
+let SequenciaCaracteres = [];
 
-       window.addEventListener('keydown', function(evento){
-          clearTimeout(Temporizador);
-          const TeclaPressionada = evento.key;
-          SequenciaCaracteres.push(TeclaPressionada);
-          const FraseCaractere = SequenciaCaracteres.join('');
+window.addEventListener('keydown', function (evento) {
+   clearTimeout(Temporizador);
+   const TeclaPressionada = evento.key;
+   SequenciaCaracteres.push(TeclaPressionada);
+   const FraseCaractere = SequenciaCaracteres.join('');
 
-          if (FraseCaractere.includes(codigoSecreto)){
-                //console.log("VOCE ACERTOOU");
+   if (FraseCaractere.includes(codigoSecreto)) {
+      //console.log("VOCE ACERTOOU");
 
-                document.querySelector('.imagem_Secreta').style.visibility = 'visible';
-                document.querySelector('.imagem_Secreta').style.opacity = 1;
+      document.querySelector('.imagem_Secreta').style.visibility = 'visible';
+      document.querySelector('.imagem_Secreta').style.opacity = 1;
 
-                Temp = setTimeout(function(){
-                  document.querySelector('.imagem_Secreta').style.visibility = 'hidden';
-                  document.querySelector('.imagem_Secreta').style.opacity = 0;
-                },2000);
-                SequenciaCaracteres = [];
-                //aumentar o tempo de visualizar
-          }
-         console.log(SequenciaCaracteres);
-         Temporizador = setTimeout(function(){
-            SequenciaCaracteres = [];
-          },1000);
-       })
+      Temp = setTimeout(function () {
+         document.querySelector('.imagem_Secreta').style.visibility = 'hidden';
+         document.querySelector('.imagem_Secreta').style.opacity = 0;
+      }, 2000);
+      SequenciaCaracteres = [];
+      //aumentar o tempo de visualizar
+   }
+   console.log(SequenciaCaracteres);
+   Temporizador = setTimeout(function () {
+      SequenciaCaracteres = [];
+   }, 1000);
+})
